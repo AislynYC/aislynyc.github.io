19d7f1d366d386f49212dd752a61f1956aad815b
AislynYC <chuang.yuchun@gmail.com>
2019-12-06 12:03:00 +0800

    api connected & rendered

---
+++ students/aislyn/stylish/js/app.js
@@ -0,0 +1,79 @@
+const rowDiv = document.getElementsByClassName('row')[0];
+const logoBtn = document.getElementsByClassName('logo')[0];
+const womanBtn = document.getElementById('nav-btn-woman');
+const manBtn = document.getElementById('nav-btn-man');
+const accBtn = document.getElementById('nav-btn-accessories');
+
+function ajax(src, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', src);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        callback(xhr.responseText);
+      } else {
+        alert(`[${xhr.status}] ${xhr.statusText}`);
+      }
+    }
+  };
+  xhr.send();
+}
+
+function render(data) {
+  const dataObj = JSON.parse(data).data;
+  Object.values(dataObj).forEach(function(item) {
+    let productDiv = document.createElement('div');
+    productDiv.className = 'col';
+    let template = `
+            <img
+              class="product-pic"
+              src=${item.main_image}
+            />
+            <div class="color-tags-area"></div>
+            <div class="product-name">${item.title}</div>
+            <div class="product-price">TWD.${item.price}</div>
+          `;
+    productDiv.innerHTML = template;
+    rowDiv.appendChild(productDiv);
+  });
+}
+
+ajax('https://api.appworks-school.tw/api/1.0/products/all', function(response) {
+  render(response);
+});
+
+logoBtn.addEventListener('click', () => {
+  rowDiv.innerHTML = '';
+  ajax('https://api.appworks-school.tw/api/1.0/products/all', function(
+    response
+  ) {
+    render(response);
+  });
+});
+
+womanBtn.addEventListener('click', () => {
+  rowDiv.innerHTML = '';
+  ajax('https://api.appworks-school.tw/api/1.0/products/women', function(
+    response
+  ) {
+    render(response);
+  });
+});
+
+manBtn.addEventListener('click', () => {
+  rowDiv.innerHTML = '';
+  ajax('https://api.appworks-school.tw/api/1.0/products/men', function(
+    response
+  ) {
+    render(response);
+  });
+});
+
+accBtn.addEventListener('click', () => {
+  rowDiv.innerHTML = '';
+  ajax('https://api.appworks-school.tw/api/1.0/products/accessories', function(
+    response
+  ) {
+    render(response);
+  });
+});
