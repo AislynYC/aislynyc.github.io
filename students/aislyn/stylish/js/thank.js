87fec78f3909364048adffee2523e764561e4fae
AislynYC <chuang.yuchun@gmail.com>
2019-12-19 17:37:10 +0800

    Complete thank page RWD layout

--- students/aislyn/stylish/js/thank.js
+++ students/aislyn/stylish/js/thank.js
@@ -1,3 +1,31 @@
 let query = window.location.search.substring(1);
 let orderNumber = query.split('=')[1];
-console.log(orderNumber);
+const cartQtyWeb = document.getElementsByClassName('cart-qty')[0];
+const cartQtyMobile = document.getElementsByClassName('cart-qty')[1];
+
+// Get Cart Data from Local Storage
+const getLocalStorage = () => {
+  if (localStorage['cart'] !== undefined) {
+    cart = JSON.parse(localStorage['cart']);
+  } else {
+    cart = {};
+  }
+};
+getLocalStorage();
+
+// Update Cart Badge
+const updateCartBadge = () => {
+  let cartCount = 0;
+
+  for (let [key, value] of Object.entries(cart)) {
+    cartCount += parseInt(value.qty);
+  }
+
+  cartQtyWeb.innerHTML = cartCount;
+  cartQtyMobile.innerHTML = cartCount;
+};
+updateCartBadge();
+
+// Show order Number
+const orderNum = document.getElementById('order-number');
+orderNum.innerHTML = orderNumber;
